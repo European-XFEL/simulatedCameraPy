8fb94a09cadcfeed334f12b93b96ca8d7e260aee
Andrea Parenti <parenti@xfel.eu>
2015-05-13 14:50:04 +0000

    First implementation using CameraInterface. Not finished yet (waiting for K1.3 release).

--- src/SimulatedCameraPy.py
+++ src/SimulatedCameraPy.py
@@ -1,98 +1,317 @@
 #!/usr/bin/env python
 
 __author__="andrea.parenti@xfel.eu"
-__date__ ="May, 2015, 04:01 PM"
+__date__ ="November 12, 2013, 11:22 AM"
 __copyright__="Copyright (c) 2010-2013 European XFEL GmbH Hamburg. All rights reserved."
 
 import time
-import sys
-import threading
+import numpy
+import scipy.misc
+import scipy.stats
+import random
+
 from karabo.device import *
-from karabo.ok_error_fsm import OkErrorFsm   # <-- choose custom FSM if any
+from karabo.camera_interface import CameraInterface
+from karabo.no_fsm import Worker
 
-
-@KARABO_CLASSINFO("SimulatedCameraPy", "1.0")
-class SimulatedCameraPy(PythonDevice, OkErrorFsm):
+@KARABO_CLASSINFO("SimulatedCameraPy", "1.3")
+class SimulatedCameraPy(PythonDevice, CameraInterface):
 
     def __init__(self, configuration):
         # always call PythonDevice constructor first!
         super(SimulatedCameraPy,self).__init__(configuration)
-        self.workerThread = None
-        self.keepWorking = False
         
+        random.seed()
+        
+        self.pollWorker = None
+        
+        self.keepAcquiring = False
+        self.swTrgReceived = False
+        
+        # Sample Image
+        self.image = None
+        
+        self.filePath = ""
+        self.fileName = ""
+        self.fileType = ""
+        self.fileCounter = 0
+        
+    def __del__(self):
+        if self.pollWorker is not None:
+            if self.pollWorker.is_running():
+                self.pollWorker.stop()
+            self.pollWorker.join()
+            self.pollWorker = None
+        
+        super(SimulatedCameraPy, self).__del__()
+    
     @staticmethod
     def expectedParameters(expected):
         '''Description of device parameters statically known'''
         (
-        INT32_ELEMENT(expected)
-                .key("startcount")
-                .displayedName("Start Count")
-                .description("Start count.")
-                .assignmentOptional().defaultValue(50)
+        STRING_ELEMENT(expected).key("imageType")
+                .displayedName("Image Type")
+                .description("Select the simulated image type")
+                .options("2d_Gaussian,RGB_Image,Grayscale_Image,Load_from_file")
+                .assignmentOptional().defaultValue("Load_from_file")
+                .init()
+                .commit()
+        ,
+        PATH_ELEMENT(expected).key("imageFilename")
+                .displayedName("Image Filename")
+                .description("The full filename to the fake image displayed by the camera. File format must be 'npy'.")
+                .assignmentOptional().defaultValue("european-xfel-logo-greyscales.npy")
+                .init()
+                .commit()
+        ,
+        DOUBLE_ELEMENT(expected).key("pixelGain")
+                .displayedName("Pixel Gain")
+                .description("The pixel gain")
+                .assignmentOptional().defaultValue(0.5)
+                .minInc(0.001)
+                .maxInc(1.000)
                 .reconfigurable()
                 .commit()
-                ,
-        INT32_ELEMENT(expected)
-                .key("countdown")
-                .displayedName("Countdown")
-                .description("Counter showing the number of yet not written dots.")
-                .assignmentOptional().defaultValue(0)
+        ,
+        STRING_ELEMENT(expected).key("cycleMode")
+                .displayedName("Cycle Mode")
+                .description("Configures whether the camera will acquire a fixed length sequence or a continuous sequence")
+                .assignmentOptional().defaultValue("Continuous")
+                .options("Fixed Continuous")
                 .reconfigurable()
+                .allowedStates("Ready")
                 .commit()
-                ,
-        STRING_ELEMENT(expected)
-                .key("karabo")
-                .displayedName("Karabo")
-                .description("Counter showing the number of yet not written dots.")
-                .assignmentOptional().defaultValue("")
+        ,
+        INT32_ELEMENT(expected).key("frameCount")
+                .displayedName("Frame Count")
+                .description("Configures the number of images to acquire in the sequence,"
+                             "when the camera is in 'Fixed' Mode")
+                .assignmentOptional().defaultValue(1)
                 .reconfigurable()
+                .allowedStates("Ready")
                 .commit()
-                ,
+        ,
+        STRING_ELEMENT(expected).key("triggerMode")
+                .displayedName("Trigger Mode")
+                .description("Allows the user to configure the camera trigger mode at a high level")
+                .assignmentOptional().defaultValue("Internal")
+                .options("Internal Software")
+                .reconfigurable()
+                .allowedStates("Ready")
+                .commit()
+        ,
+        ###################################
+        #  READ ONLY HARDWARE PARAMETERS  #
+        ###################################
+        DOUBLE_ELEMENT(expected).key("sensorTemperature")
+                .displayedName("Sensor Temperature")
+                .description("Returns the temperature of the sensor in Celsius degrees")
+                .unit(Unit.DEGREE_CELSIUS)
+                .readOnly()
+                .commit()
+        ,
+        INT32_ELEMENT(expected).key("sensorHeight")
+                .displayedName("Sensor Height")
+                .description("Returns the height of the sensor in pixels")
+                .readOnly()
+                .commit()
+        ,
+        INT32_ELEMENT(expected).key("sensorWidth")
+                .displayedName("Sensor Width")
+                .description("Returns the width of the sensor in pixels")
+                .readOnly()
+                .commit()
+        ,
+        BOOL_ELEMENT(expected).key("cameraAcquiring")
+                .displayedName("Camera Acquiring")
+                .description("Returns whether or not an acquisition is currently running")
+                .readOnly()
+                .commit()
+        ,
+        STRING_ELEMENT(expected).key("cameraModel")
+                .displayedName("Camera Model")
+                .description("Returns the camera model")
+                .readOnly()
+                .commit()
+        ,
         )
-
-    ##############################################
-    #   Implementation of State Machine methods  #
-    ##############################################
-
-    def okStateOnEntry(self):
-        '''Start thread with user business logic.'''
-        #raise ValueError,"Introduced artificial exception :)"
-        self.workerThread = threading.Thread(target=self.mainWork, name="MainWork")
-        self.workerThread.start()
-        self.log.INFO("Device running...")
+    
+    def preReconfigure(self, inputConfig):
+        if inputConfig.has("pollInterval") and self.pollWorker is not None:
+            timeout = 1000 * inputConfig.get("pollInterval") # to milliseconds
+            self.pollWorker.setTimeout(timeout)
         
-    def okStateOnExit(self):
-        '''Stop worker thread.'''
-        try:
-            self.keepWorking = False
-            if self.workerThread is not None and self.workerThread.is_alive():
-                self.workerThread.join()
-        except Exception, e:
-            self.log.ERROR("Exception while joining worker thread", str(e))
-        else:
-            self.log.INFO("Device stopped...")
+    def initialize(self):
+        self.updateState("Initializing")
         
-    def mainWork(self):
-        '''Put here business logic.'''
+        # Camera model
+        self.set("cameraModel", "simCam")
+        
+        imageType = self.get("imageType")
+        
         try:
-            self["karabo"] = "Attention, ple-e-e-ase!)"
-            self["countdown"] = self["startcount"]
-            self.keepWorking = True
-            while self.keepWorking:
-                if self["countdown"] == 0:
-                    raise ValueError,"Exception raised intentionally! :)"
-                sys.stdout.write('.')
-                sys.stdout.flush()
-                self["countdown"] = self["countdown"] - 1  # set into device and report changes to GUI
-                time.sleep(0.1)
-        except Exception, e:
-            self.execute("errorFound", "Exception", str(e))  # goto Error state
-            self["karabo"] = "B-O-O-O-M-M-S-S!!! :("
-            self.log.INFO("Exit mainWork()...")
+            if imageType == "2d_Gaussian":
+                # 2d Gaussian, no rotation
+                rvx = scipy.stats.norm(300, 50)
+                x = rvx.pdf(numpy.arange(800)) # 1d gaussian
+                rvy = scipy.stats.norm(200, 75)
+                y = rvy.pdf(numpy.arange(600)) # 1d gaussian
+                z = numpy.outer(y, x) # 2d gaussian (float64)
+                data = (z/z.max()*0.5*numpy.iinfo('uint16').max).astype('uint16') # -> uint16
+                self.log.INFO('2d gaussian image loaded')
+            elif imageType == 'RGB_Image':
+                # RGB image
+                data = numpy.append(numpy.append([[255,0,0]*67500], [[0,255,0]*67500]), [[0,0,255]*67500]).reshape(450,450,3).astype('uint16')
+                self.log.INFO('RGB image loaded')
+            elif imageType == 'Load_from_file':
+                # Try to load image file
+                filename = self.get("imageFilename")
+                data = numpy.load(filename)
+                self.log.INFO('Image loaded from file ' + filename)
+            else:
+                # Default image, grayscale, vertical gradient
+                a = numpy.arange(500, dtype=numpy.uint16)
+                b = numpy.array([a]*500)
+                data = numpy.rot90(b)
+                self.log.INFO('Default image (grayscale) loaded')
+        except Exception as e:
+            # Default image, grayscale, vertical gradient
+            a = numpy.arange(500, dtype=numpy.uint16)
+            b = numpy.array([a]*500)
+            data = numpy.rot90(b)
+            self.log.WARN(str(e))
+            self.log.INFO('Default image (grayscale) loaded')
+        
+        self.image = data
+        
+        # Sensor geometry
+        self.set("sensorHeight", data.shape[0])
+        self.set("sensorWidth", data.shape[1])
+        
+        if self.pollWorker is None:
+            # Create and start poll worker
+            timeout = 1000*self.get("pollInterval") # to milliseconds
+            self.pollWorker = Worker(self.pollHardware, timeout, -1).start()
+        
+        # Sleep a while, then go to "Ready"
+        time.sleep(1)
+        self.updateState("Ready")
+    
+    def acquire(self):
+        self.updateState("Acquiring")
+        self.set("cameraAcquiring", True)
+        
+        # Is software trigger?
+        triggerModeIsSoftware = False
+        if self.get("triggerMode") == "Software":
+            triggerModeIsSoftware = True
+        
+        # Is cycle mode "fixed"?
+        cycleModeIsFixed = False
+        if self.get("cycleMode") == "Fixed":
+            cycleModeIsFixed = True
+        # How many frames should be acquired
+        frameCount = self.get("frameCount")
+        # Frame counter
+        frames = 0
+        
+        # Get file path, name and extension
+        newName = False
+        if self.get("imageStorage.filePath")!= self.filePath:
+            # New file path
+            self.filePath = self.get("imageStorage.filePath")
+            newName = True
+        if self.get("imageStorage.fileName")!= self.fileName:
+            # New file root name
+            self.fileName = self.get("imageStorage.fileName")
+            newName = True
+        if self.get("imageStorage.fileType")!= self.fileType:
+            # New file extension
+            self.fileType = self.get("imageStorage.fileType")
+        
+        if newName:
+            # File has changed name... reset counter
+            self.fileCounter = 0
+        
+        saveImages = self.get("imageStorage.enable")
+        pixelGain = self.get("pixelGain")
+        image = self.image # Copy original image
+        image *= pixelGain # Apply pixel gain to copy
+        
+        self.keepAcquiring = True
+        self.swTrgReceived = False
+        
+        while self.keepAcquiring:
             
+            exposureTime = self.get("exposureTime")
+            newPixelGain = self.get("pixelGain")
+            
+            if triggerModeIsSoftware:
+                # Running in SW trigger mode
+                if self.swTrgReceived:
+                    # A SW trigger has been received
+                    self.swTrgReceived = False # Reset the flag, "acquire" image
+                else: 
+                    # No SW trigger yet... will check again in 10 ms
+                    time.sleep(0.010)
+                    continue
+            
+            # Sleep for "exposureTime"
+            time.sleep(exposureTime)
+            
+            # Pixel gain has changed
+            if newPixelGain!=pixelGain:
+                image *= newPixelGain/pixelGain
+                pixelGain = newPixelGain
+            
+            # Construct image data object in parts
+            imageData = ImageData()
+            if image.ndim==2:
+                dims = Dims(image.shape[0], image.shape[1]) # TODO: new API
+                imageData.setData(image, True)
+                imageData.setDimensions(dims);
+            elif image.ndim>2:
+                # Color image (TODO)
+                pass
 
-    # Put here more state machibe actioins if needed... . See FSM API
-   
+            # self.write("output", "image", imageData) # TODO new API
+            print("Received new image") # TODO rm
+            
+            if saveImages:
+                # Create filename (without path and extension)
+                imgname = "%s%06ld" % (self.fileName, self.fileCounter)
+                # Prepend path and append extension
+                imgname = os.path.join(self.filePath, imgname + "." + self.fileType)
+                
+                if self.fileType=="tif" or self.fileType=="jpg" or self.fileType=="png":
+                    # PIL must installed!
+                    scipy.misc.imsave(imgname, image)
+                else:
+                    raise ValueError("File type not supported")
+
+                self.set("imageStorage.lastSaved", imgname)
+                self.fileCounter+=1
+            
+            frames+=1
+            if cycleModeIsFixed and frames>=frameCount:
+                self.stop()
+    
+    def trigger(self):
+         self.swTrgReceived = True
+    
+    def stop(self):
+        self.keepAcquiring = False
+        self.set("cameraAcquiring", False)
+
+        self.updateState("Ready")
+    
+    def resetHardware(self):
+        self.updateState("Ready")
+    
+    def pollHardware(self, isRepetitionCounterExpired):
+        temperature = 25.4 + random.random()/10.
+        self.set("sensorTemperature", temperature);
+    
 # This entry used by device server
 if __name__ == "__main__":
     launchPythonDevice()
